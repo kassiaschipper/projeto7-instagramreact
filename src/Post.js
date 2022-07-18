ac8f58f7a98b24fec222dd1c34213ed7e4fc9496
Kássia <kassia_guterres@outlook.com>
2022-07-18 15:19:06 -0300

    bonus - inserção de video em post com like

--- src/Post.js
+++ src/Post.js
@@ -1,139 +1,108 @@
-import React from "react";
-import Icone from "./Icone";
+import React, { useEffect, useRef } from "react";
 
 export default function Post(props) {
+  const [curtida, SetCurtida] = React.useState("heart-outline");
+  const [classe, setClasse] = React.useState("sem-like");
 
-    const [curtida, SetCurtida] = React.useState('heart-outline');
-    const [classe, setClasse] = React.useState("sem-like");
+  const videoEl = useRef(null);
 
-    return (
-        <div class="post">
-            <div class="topo">
-                <div class="usuario">
-                    <img src={props.url} />
-                    {props.nome}
-                </div>
-                <div class="acoes">
-                    <ion-icon name="ellipsis-horizontal"></ion-icon>
-                </div>
-            </div>
-            <div class="conteudo">
-                <img src={props.img} onClick={() => {
-                            if (curtida === "heart-outline") {
-                                SetCurtida("heart")
-                                setClasse("preenchido")
-                            } else {
-                                SetCurtida("heart-outline")
-                                setClasse("sem-like")
-                            }
-                        }}/>
-            </div>
+  const attemptPlay = () => {
+    videoEl &&
+      videoEl.current &&
+      videoEl.current.play().catch((error) => {
+        console.error("Error attempting to play", error);
+      });
+  };
+  useEffect(() => {
+    attemptPlay();
+  }, []);
 
-            <div class="fundo">
-                <div class="acoes">
-                    <div>
-                        <ion-icon name={curtida} class={classe} onClick={() => {
-                            if (curtida === "heart-outline") {
-                                SetCurtida("heart")
-                                setClasse("preenchido")
-                            } else {
-                                SetCurtida("heart-outline")
-                                setClasse("sem-like")
-                            }
-                        }}></ion-icon>
-                        <ion-icon name="chatbubble-outline"></ion-icon>
-                        <ion-icon name="paper-plane-outline"></ion-icon>
-                    </div>
-                    <div>
-                        <ion-icon name="bookmark-outline"></ion-icon>
-                    </div>
-                </div>
+  let media =
+    props.img === "" ? (
+      <video
+        autoPlay
+        playsInline
+        muted
+        loop
+        controls
+        src={props.video}
+        ref={videoEl}
+        style={{ maxWidth: "100%", width: "800px", margin: "0 auto" }}
+        onClick={() => {
+          if (curtida === "heart-outline") {
+            SetCurtida("heart");
+            setClasse("preenchido");
+          } else {
+            SetCurtida("heart-outline");
+            setClasse("sem-like");
+          }
+        }}
+      />
+    ) : (
+      <img
+        src={props.img}
+        onClick={() => {
+          if (curtida === "heart-outline") {
+            SetCurtida("heart");
+            setClasse("preenchido");
+          } else {
+            SetCurtida("heart-outline");
+            setClasse("sem-like");
+          }
+        }}
+      />
+    );
 
-                <div class="curtidas">
-                    <img src={props.curtiu} />
-                    <div class="texto">
-                        Curtido por <strong>{props.perfil}</strong> e <strong>outras {props.likes} pessoas</strong>
-                    </div>
-                </div>
-            </div>
+  //
+  return (
+    <div class="post">
+      <div class="topo">
+        <div class="usuario">
+          <img src={props.url} />
+          {props.nome}
         </div>
-    )
+        <div class="acoes">
+          <ion-icon name="ellipsis-horizontal"></ion-icon>
+        </div>
+      </div>
+      <div class="conteudo">
+        {media}
+        
+      </div>
 
+      <div class="fundo">
+        <div class="acoes">
+          <div>
+            <ion-icon
+              name={curtida}
+              class={classe}
+              onClick={() => {
+                if (curtida === "heart-outline") {
+                  SetCurtida("heart");
+                  setClasse("preenchido");
+                } else {
+                  SetCurtida("heart-outline");
+                  setClasse("sem-like");
+                }
+              }}
+            ></ion-icon>
+            <ion-icon name="chatbubble-outline"></ion-icon>
+            <ion-icon name="paper-plane-outline"></ion-icon>
+          </div>
+          <div>
+            <ion-icon name="bookmark-outline"></ion-icon>
+          </div>
+        </div>
 
+        <div class="curtidas">
+          <img src={props.curtiu} />
+          <div class="texto">
+            Curtido por <strong>{props.perfil}</strong> e{" "}
+            <strong>outras {props.likes} pessoas</strong>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//     const postagem = [
-//         { url: "assets/img/meowed.svg", pagina: "meowed", imagemPost: "assets/img/gato-telefone.svg", ulrCurtidas: "assets/img/respondeai.svg", curtiu: "respondeai", quantidadeCurtidas: "101.523" },
-//         { url: "assets/img/barked.svg", pagina: "barked", imagemPost: "assets/img/dog.svg", ulrCurtidas: "assets/img/adorable_animals.svg", curtiu: "adorable_animals", quantidadeCurtidas: "99.159" }
-
-//     ];
-
-
-//     return (
-//         <>
-     
-//        { postagem.map(value => (
-//                 <div className="post" >
-//                     <div className="topo">
-//                         <div className="usuario">
-//                             <img src={value.url} />
-//                             {value.pagina}
-//                         </div>
-//                         <div className="acoes">
-//                             <Icone name="ellipsis-horizontal" />
-//                         </div>
-//                     </div>
-
-//                     <div className="conteudo">
-//                         <img src={value.imagemPost} />
-//                     </div>
-
-//                     <div className="fundo">
-//                         <div className="acoes">
-//                             <div>
-//                                 <Icone className="preenchido" name="heart" />
-//                                 <Icone name="chatbubble-outline" />
-//                                 <Icone name="paper-plane-outline" />
-//                             </div>
-//                             <div>
-//                                 <Icone name="bookmark-outline" />
-//                             </div>
-//                         </div>
-
-//                         <div className="curtidas">
-//                             <img src={value.ulrCurtidas} />
-//                             <div className="texto">
-//                                 Curtido por <strong>{value.curtiu}</strong> e <strong>outras {value.quantidadeCurtidas} pessoas</strong>
-//                             </div>
-//                         </div>
-//                     </div>
-//                 </div>
-
-//        ))
-//         }
-//         </>
-
-//     );
-// }
--- src/Posts.js
+++ src/Posts.js
@@ -3,15 +3,16 @@ import Post from "./Post";
 export default function Posts() {
 
     const postagem = [
-        {url: "assets/img/meowed.svg", img: "assets/img/gato-telefone.svg", nome: "meowed", curtiu: "assets/img/respondeai.svg", perfil: "respondeai", likes: "101.523"}, 
-        {url: "assets/img/barked.svg", img: "assets/img/dog.svg", nome: "barked", curtiu: "assets/img/adorable_animals.svg", perfil: "adorable_animals", likes: "99.159"}
+        {url: "assets/img/meowed.svg", img: "assets/img/gato-telefone.svg", nome: "meowed", curtiu: "assets/img/respondeai.svg", perfil: "respondeai", likes: "101.523", video:""}, 
+        {url: "assets/img/barked.svg", img: "assets/img/dog.svg", nome: "barked", curtiu: "assets/img/adorable_animals.svg", perfil: "adorable_animals", likes: "99.159", video:""},
+        {url: "assets/img/catanacomics.svg", img: "", nome: "catanacomics", curtiu: "assets/img/9gag.svg", perfil: "9gag", likes: "105.00", video:"assets/video/video.mp4"}
     ]
 
     return (
         <div class="posts">
 
             {postagem.map((value) =>
-                <Post url={value.url} img={value.img} nome={value.nome} curtiu={value.curtiu} perfil={value.perfil} likes={value.likes} />
+                <Post url={value.url} img={value.img} nome={value.nome} curtiu={value.curtiu} perfil={value.perfil} likes={value.likes} video={value.video} />
             )}
         </div>
     )
